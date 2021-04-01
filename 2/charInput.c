ad97fb4f57e94e123de69db1142a7b64c5dea686
Neethika <neethikasumesh@gmail.com>
2021-04-01 09:46:08 +0530

    sorting and charInput finished

---
+++ 2/charInput.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main(){
+
+    char choice;
+
+    printf("enter y/n");
+    scanf("%c",&choice);
+
+    if (choice=='y')
+    {
+        printf("helloworld");
+    }
+    else
+    {
+        printf("no helloworld");
+    }
+    
+}
